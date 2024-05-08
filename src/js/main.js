7d89821a4afb0c367c1710bc045d72963ff5cb60
dmit-s <dwhatly@gmail.com>
2024-05-08 11:56:30 +0300

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 const enterBtn = document.getElementById("enter-btn");
 function handleClick() {
-  window.location.href = "http://localhost:5173/room";
+  window.location.href = `${import.meta.env.VITE_SERVER_URL}/room`;
 }
 
 enterBtn.addEventListener("click", handleClick);
